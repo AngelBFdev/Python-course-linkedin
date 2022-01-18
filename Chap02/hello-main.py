68b129a2c08c9ddb09823732654bd9420908383a
Anisbeflo <abejarano0@ucol.mx>
2022-01-18 10:55:56 -0600

    Example of if statement

--- Chap02/hello-main.py
+++ Chap02/hello-main.py
@@ -8,6 +8,11 @@ def main():
 
 def message():
     print('This is python version {}'.format(platform.python_version()))
+    print('line 2')
+    if False:               #This is a comment
+        print('line 3')     #This is another comment
+    else:
+        print('not true')
 
 # This says, that if it runs from here, to execute main function
 if __name__ == '__main__': main()
