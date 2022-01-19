b3880b67c191f043edb1d2e49abeda00edc32948
Anisbeflo <abejarano0@ucol.mx>
2022-01-19 13:45:57 -0600

    Explanations
    - Explanation of input method
    - Explanation of continue in loops
    - Explanation of else after loop

--- Chap06/while.py
+++ Chap06/while.py
@@ -3,6 +3,21 @@
 
 secret = 'swordfish'
 pw = ''
+auth = False
+count = 0
+max_attempt = 5
 
 while pw != secret:
-    pw = input("What's the secret word? ")
+    # input to get text
+    count +=1
+    if count > max_attempt: break
+    # continue skip this case like next in ruby
+    if count == 3: continue
+    pw = input(f"{count}What's the secret word? ")
+
+# else statement after a loop will only be
+# executed when finished correctly
+else:
+    auth=True
+
+print("Authorized" if auth else "Calling the FBI ...")
