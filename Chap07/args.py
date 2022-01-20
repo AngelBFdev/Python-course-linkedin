1ad0e061b11726f01f4081a3c2cb0ed059b0273c
Anisbeflo <abejarano0@ucol.mx>
2022-01-20 10:25:40 -0600

    Explanation of args in functions

--- Chap07/args.py
+++ Chap07/args.py
@@ -2,9 +2,17 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
+    x = ('Hello', 'World')
     kitten('meow', 'grrr', 'purr')
+    # without * make it just a variable list
+    # * send the data in the list as
+    # a group of variables
+    kitten(*x)
 
+
+# *args send list
 def kitten(*args):
+    # runs if you have any argument
     if len(args):
         for s in args:
             print(s)
