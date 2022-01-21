b25d226a01f958aa9422893b70e265ff5bcb9a9c
Anisbeflo <abejarano0@ucol.mx>
2022-01-21 11:41:25 -0600

    Explanation of how the code work
    - Information about recursive code
    - Information about repr() method from print

--- Chap08/mixed.py
+++ Chap08/mixed.py
@@ -16,14 +16,25 @@ def main():
 def disp(o):
     global dlevel
 
+    # 1st step: o = [ l, r, s, d, t ]
+    # 2nd step: o = [ 1, 'two', 3, {'4': 'four' }, 5 ]
+    # 3rd step: o = 1
+    # 1st step: dlevel = 1
+    # 2nd step: dlevel = 2
     dlevel += 1
+    # 1st step: send it to print_list
+    # 2nd step: send it to print_list
     if   isinstance(o, list):  print_list(o)
     elif isinstance(o, range): print_list(o)
     elif isinstance(o, tuple): print_tuple(o)
     elif isinstance(o, set):   print_set(o)
     elif isinstance(o, dict):  print_dict(o)
     elif o is None: print('Nada', end=' ', flush=True)
+    # if is not any from above it would print it
+    # method repr print with characters, example
+    # 'Hola' instead of Hola
     else: print(repr(o), end=' ', flush=True)
+    # dlevel = 2 now, minus 1 now is 1
     dlevel -= 1
 
     if dlevel <= 1: print() # newline after outer
