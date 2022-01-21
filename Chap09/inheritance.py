68709948b993fc2db583fca0631e83dbb59a852f
Anisbeflo <abejarano0@ucol.mx>
2022-01-21 13:16:21 -0600

    Explanation
    - Object father
    - Methods from objects sons
    - Try Catch

--- Chap09/inheritance.py
+++ Chap09/inheritance.py
@@ -9,6 +9,7 @@ class Animal:
 
     def type(self, t = None):
         if t: self._type = t
+        # Try catch in python
         try: return self._type
         except AttributeError: return None
 
@@ -28,11 +29,21 @@ class Duck(Animal):
         if 'type' in kwargs: del kwargs['type']
         super().__init__(**kwargs)
 
+# set the father class
 class Kitten(Animal):
     def __init__(self, **kwargs):
+        #set type in class kitten
         self._type = 'kitten'
+        # delete the type if it was typed
+        # in the dictionary
         if 'type' in kwargs: del kwargs['type']
+        # send it to the father class
         super().__init__(**kwargs)
+
+    # This method is only available with
+    # the object kitten
+    def kill(self,s = 'humans'):
+        print(f'{self.name()} will now kill all {s}!')
 
 def print_animal(o):
     if not isinstance(o, Animal):
@@ -44,5 +55,7 @@ def main():
     a1 = Duck(name = 'donald', sound = 'quack')
     print_animal(a0)
     print_animal(a1)
+    a0.kill('dogs')
+    a0.kill()
 
 if __name__ == '__main__': main()
