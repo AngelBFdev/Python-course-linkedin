c6af1f8de3014e4d061b9ff66d5b5348d3c08ce6
Anisbeflo <abejarano0@ucol.mx>
2022-01-21 15:33:24 -0600

    Explanation of string methods and how to use format in variable string

---
+++ Chap11/string-methods.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+# All capital letters
+print('Hello, Wold.'.upper())
+
+# All lowercase letters
+print('Hello, Wold.'.lower())
+
+# Capitalize letters
+print('Hello, Wold.'.capitalize())
+
+# Capilize all first letters
+print('Hello, Wold.'.title())
+
+# Swap the capitalized letters
+print('Hello, Wold.'.swapcase())
+
+# similar to lower but it
+# lowercase other languages
+print('Hello, Wold.'.casefold())
+
+# you can concat literal strings like this
+s3 = 'this string' ' that string'
+print(s3)
+
+# You can find more methods from
+# the documentation of string in python
---
+++ Chap11/string-objects.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+# Method that print backwards
+# More info in: Chap09/string.py
+class MyString(str):
+  def __str__(self):
+    return self[::-1]
+
+s = MyString('Hello, World.')
+print(s)
+
+# You can add the format later
+s = 'Hello, Wold. {}'
+print(s.format(42 * 7))
+print(s.format(42 * 8))
