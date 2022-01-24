d71d5a9a863ca38221e1c7cf826a65b68f897f05
Anisbeflo <abejarano0@ucol.mx>
2022-01-23 21:10:56 -0600

    Explanation of method split and join

--- Chap11/split-join.py
+++ Chap11/split-join.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+s = 'This is    a long string with    a bunch   of words in it.'
+# split method by itself, separate the string
+# by the spaces
+print(s.split())
+
 s = 'This is a long string with a bunch of words in it.'
-print(s)
+# You can choose specific characters to split
+# with
+print(s.split('i'))
+
+l=s.split()
+# This is the way to apply join in python.
+s2= ':'.join(l)
+print(s2)
