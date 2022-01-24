e5a4163162c367e68b0e46c5513883ab4c28e7cb
Anisbeflo <abejarano0@ucol.mx>
2022-01-24 13:13:05 -0600

    Explanation (sqlite)
    - create/open a database
    - create table
    - insert row
    - read all rows
    - drop table
    - close database

--- Chap15/db-api.py
+++ Chap15/db-api.py
@@ -5,16 +5,24 @@ import sqlite3
 
 def main():
     print('connect')
+    # connect to the database name db-api.db
+    # if doesn't exist create one
     db = sqlite3.connect('db-api.db')
+    # with this we can execute sql consults
     cur = db.cursor()
+
     print('create')
+    # drop the table if already exist
     cur.execute("DROP TABLE IF EXISTS test")
+    # create table name test with id, string and number
     cur.execute("""
         CREATE TABLE test (
             id INTEGER PRIMARY KEY, string TEXT, number INTEGER
         )
         """)
+
     print('insert row')
+    # insert in test table, values: string='one', number=1
     cur.execute("""
         INSERT INTO test (string, number) VALUES ('one', 1)
         """)
@@ -26,18 +34,27 @@ def main():
     cur.execute("""
         INSERT INTO test (string, number) VALUES ('three', 3)
         """)
+
     print('commit')
+    # we commit the inserts
     db.commit()
+
     print('count')
     cur.execute("SELECT COUNT(*) FROM test")
+    # save the result of the last query
     count = cur.fetchone()[0]
     print(f'there are {count} rows in the table.')
+
     print('read')
+    # print all rows
     for row in cur.execute("SELECT * FROM test"):
         print(row)
+
     print('drop')
     cur.execute("DROP TABLE test")
+
     print('close')
+    # close the table
     db.close()
 
 if __name__ == '__main__': main()
