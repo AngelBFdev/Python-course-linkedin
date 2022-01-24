56389972f371232dec8cfaea27a50bc30c3f26b5
Anisbeflo <abejarano0@ucol.mx>
2022-01-23 21:26:44 -0600

    Explanation
    - How to open a file to: write, read and append.
    - rstrip string method: remove spaces at the end of a string

--- Chap12/files.py
+++ Chap12/files.py
@@ -2,8 +2,17 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 def main():
+    # open a file
+    # ('name_file', 'w') -> this will write
+    # from ZERO
+    # ('name_file', 'a') -> this will write
+    # continuing from the end
+    # ('name_file', 'r+') -> allow you to read
+    # and write
     f = open('lines.txt')
     for line in f:
+        # Read a string and remove spaces
+        # at the end of the string
         print(line.rstrip())
 
 if __name__ == '__main__': main()
