454d90e57bcf0232ecb3a84eed6943fcf51a6f10
Anisbeflo <abejarano0@ucol.mx>
2022-01-24 10:16:28 -0600

    Explanations
    - files: Fix path error, by using Path from the pathlib. More detail in
      the script
    - copy-text: Fix path error, and how to write a file

--- Chap12/copy-text.py
+++ Chap12/copy-text.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+from pathlib import Path
+
 def main():
-    infile = open('lines.txt', 'rt')
-    outfile = open('lines-copy.txt', 'wt')
+    # Got the Path for the parent carpet of the script
+    script_location = Path(__file__).absolute().parent
+
+    # write the route and open it for read in text mode
+    infile = open(f'{script_location}/lines.txt', 'rt')
+
+    # write the route and open it for write in text mode
+    # write mode clean the file and start writing
+    outfile = open(f'{script_location}/lines-copy.txt', 'wt')
     for line in infile:
+        # write the line from infile into the outfile
         print(line.rstrip(), file=outfile)
+
+        # Also save the lines, but do not strip
+        # the spaces at the end
+        outfile.writelines(line)
+
+        # print '.' for each line
+        # flush start writing dots without
+        # needing a new line
         print('.', end='', flush=True)
+
+    # Close the file in order to do not
+    # lose data
     outfile.close()
     print('\ndone.')
 
--- Chap12/files.py
+++ Chap12/files.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+from pathlib import Path
+
 def main():
+    # Got the Path for the parent carpet of the script
+    script_location = Path(__file__).absolute().parent
+    # write the route
+    file_location = script_location / 'lines.txt'
+    # just to verify the route
+    print(file_location)
     # open a file
     # ('name_file', 'w') -> this will write
     # from ZERO
@@ -9,7 +17,7 @@ def main():
     # continuing from the end
     # ('name_file', 'r+') -> allow you to read
     # and write
-    f = open('lines.txt')
+    f = open(file_location)
     for line in f:
         # Read a string and remove spaces
         # at the end of the string
