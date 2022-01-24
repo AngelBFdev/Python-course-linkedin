d1fb76d3236b0cd7de97938bbbec74334d7682b5
Anisbeflo <abejarano0@ucol.mx>
2022-01-24 10:26:13 -0600

    How to write binary files (images is a binary file)

--- Chap12/copy-bin.py
+++ Chap12/copy-bin.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+from pathlib import Path
+
 def main():
-    infile = open('berlin.jpg', 'rb')
-    outfile = open('berlin-copy.jpg', 'wb')
+    # Got the path
+    script_location = Path(__file__).absolute().parent
+
+    # Open the file in read and binary mode
+    infile = open(f'{script_location}/berlin.jpg', 'rb')
+
+    # Open the file in write and binary mode
+    outfile = open(f'{script_location}/berlin-copy.jpg', 'wb')
+
     while True:
+        # read 10240 bytes for the buffer
         buf = infile.read(10240)
         if buf:
+            # write in the outfile
             outfile.write(buf)
             print('.', end='', flush=True)
         else: break
